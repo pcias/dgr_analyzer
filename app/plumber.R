050f97f3e8dfcce4009ce1ddfdf5eccad5133deb
Przemyslaw Cias <przemyslaw.cias@gmail.com>
2021-02-20 13:29:36 +0100

    entry points for long and short positions

--- app/plumber.R
+++ app/plumber.R
@@ -29,6 +29,31 @@ function(sample_url) {
 }
 
 
+#* provide summary of long positions
+#* @param sample_url sample_url from open session (portfel->pobierz->xls->copy link address)
+#* @get /long_positions
+#* @serializer htmlwidget
+function(sample_url) {
+  psb <- pSBmap(sample_url)
+  output <- pSBreactable(psb)
+  
+  return(output$long)
+}
+
+#* provide summary of short positions
+#* @param sample_url sample_url from open session (portfel->pobierz->xls->copy link address)
+#* @get /short_positions
+#* @serializer htmlwidget
+function(sample_url) {
+  psb <- pSBmap(sample_url)
+  output <- pSBreactable(psb)
+  
+  return(output$short)
+}
+
+
+
+
 #* fake (testing) summary for closed positions
 #* @param sample_url sample_url from open session (portfel->pobierz->xls->copy link address)
 #* @get /fakeit
