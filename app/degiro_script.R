74fda461f41ddd29c2e92761f919b136cbb24d21
Przemek Cias <przemyslaw.cias@gmail.com>
2021-03-06 14:16:43 +0100

    license

--- app/degiro_script.R
+++ app/degiro_script.R
@@ -1,3 +1,21 @@
+# Copyright (C) 2021 pcs Przemyslaw Cias
+# 
+# This file is part of DGR Analyzer.
+# 
+# DGR Analyzer is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# DGR Analyzer-React is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with DGR Analyzer-React.  If not, see <http://www.gnu.org/licenses/>.
+
+
 library(rio)
 #library(writexl)
 library(urltools)
